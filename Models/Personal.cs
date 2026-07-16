119d06b18983a11e8a5fdc6fff8dd68359fee8a5
Gradi Puata <gradipuata@gmail.com>
2026-07-16 14:09:18 +0200

    added Personal model

---
+++ Models/Personal.cs
@@ -0,0 +1,24 @@
+using GradiApi.Utils;
+
+namespace GradiApi.Models
+{
+  class Personal
+  {
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public string Bio { get; set; } = string.Empty;
+    public List<string> Hobbies { get; set; }
+    public List<string> Skills { get; set; }
+    public List<string> TechStack { get; set; }
+    public List<string> ProgrammingLanguages { get; set; }
+    public Socials? Socials { get; set; }
+
+
+
+
+  }
+
+}
---
+++ Utils/Socials.cs
@@ -0,0 +1,8 @@
+namespace GradiApi.Utils
+{
+  class Socials
+  {
+    public string? Platform { get; set; }
+    public string? Link { get; set; }
+  }
+}
