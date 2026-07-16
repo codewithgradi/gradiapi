5267e0baf9193ba0d478074062067e9b98d932e1
Gradi Puata <gradipuata@gmail.com>
2026-07-16 14:42:54 +0200

    added app db context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+using GradiApi.Models;
+using Microsoft.EntityFrameworkCore;
+namespace GradiApi.Data
+{
+  class AppDbContext : DbContext
+  {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+    public DbSet<Experience> Experiences { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<Personal> Personal { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+
+  }
+};
