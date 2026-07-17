0e06e911494f6f1d0afe219f227429a4b6579a0e
Gradi Puata <gradipuata@gmail.com>
2026-07-17 08:59:38 +0200

    added global exception

--- Middlewares/GlobalExceptionMiddleware.cs
+++ Middlewares/GlobalExceptionMiddleware.cs
@@ -1,4 +1,7 @@
 using System.Net;
+using System.Text.Json;
+using GradiApi.Exceptions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GradiApi.Middlewares;
 
@@ -26,9 +29,60 @@ public class GlobalExceptionMiddleware : IMiddleware
   private async Task HandleExceptionAsync(HttpContext context, Exception e)
   {
     context.Response.ContentType = "application/json";
+
     var statusCode = HttpStatusCode.InternalServerError;
     var title = "server error";
     var message = "A serious system error occured on the server";
 
+    switch (e)
+    {
+      case BusinessRuleException:
+        statusCode = HttpStatusCode.BadRequest;
+        title = "Bad request";
+        message = e.Message;
+        break;
+      case ReasourceNotFoundException:
+        statusCode = HttpStatusCode.NotFound;
+        title = "Not Found";
+        message = e.Message;
+        break;
+      case ReasourceConflictException:
+        statusCode = HttpStatusCode.Conflict;
+        title = "Conflict";
+        message = e.Message;
+        break;
+      case UnauthorizedException:
+        statusCode = HttpStatusCode.Unauthorized;
+        title = "Unauthorized";
+        message = e.Message;
+        break;
+      default:
+        break;
+
+    }
+    context.Response.StatusCode = (int)statusCode;
+    var res = new ProblemDetails
+    {
+      Status = (int)statusCode,
+      Title = title,
+      Detail = message,
+      Instance = context.Request.Path
+
+    };
+
+    var jsonOptions = new JsonSerializerOptions
+    {
+      PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+    await
+     context
+    .Response
+    .WriteAsync(
+      JsonSerializer
+      .Serialize(
+        res,
+        jsonOptions)
+        );
   }
 }
+
