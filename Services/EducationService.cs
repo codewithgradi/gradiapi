568f356dc1874cedaae08f86e447d03a8b558cfb
Gradi Puata <gradipuata@gmail.com>
2026-07-22 10:42:06 +0200

    added education services

--- Services/EducationService.cs
+++ Services/EducationService.cs
@@ -1,18 +1,33 @@
 using GradiApi.DTO;
+using GradiApi.Mappings;
 namespace GradiApi.Services;
 
 public class EducationService
 {
-  public Task<List<GetEducationDto>> Get()
+  private readonly IEducationRepo _repo;
+  private readonly EducationMappers _mapper;
+
+  public EducationService(IEducationRepo repo, EducationMappers mapper)
   {
+    _repo = repo;
+    _mapper = mapper;
+  }
+
+  public async Task<List<GetEducationDto>> Get()
+  {
+    var educations = await _repo.GetEducation();
+    return educations.Select(x => _mapper.MapToGet(x)).ToList();
+  }
+  public async Task<GetEducationDto> Post(PostEducationDto dto, int id)
+  {
+    var model = _mapper.MapToEntity(dto);
+    var created = await _repo.CreateEducation(model, id);
+    return _mapper.MapToGet(created);
 
   }
-  public Task<GetEducationDto> Post()
+  public async Task<GetEducationDto> Put(PostEducationDto dto, int id)
   {
-
-  }
-  public Task<GetEducationDto> Put()
-  {
-
+    var result = await _repo.UpdateEducation(dto, id);
+    return _mapper.MapToGet(result);
   }
 }
